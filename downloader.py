a9f9e6b2378d5c621451fc60548cd4dae5294cb6
miist1980 <bissmilaziggurat@gmail.com>
2024-10-09 11:18:57 -0700

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,22 @@
+import youtube_dl
+
+
+def download():
+    video_url = input("please enter youtube video url:")
+    video_info = youtube_dl.YoutubeDL().extract_info(url=video_url,
+                                                     download=False)
+    filename = f"downloads/{video_info['title']}.mp3"
+    options = {
+        'format': 'bestaudio/best',
+        'keepvideo': False,
+        'outtmpl': filename,
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    print("Download complete... {}".format(filename))
+
+
+if __name__ == '__main__':
+    download()
